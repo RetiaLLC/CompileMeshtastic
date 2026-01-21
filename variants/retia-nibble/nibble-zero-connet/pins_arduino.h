69fd91be7313cc60c62a6f16723ebd1d8100381c
Skickar <40251293+skickar@users.noreply.github.com>
2026-01-21 02:00:28 -0800

    Refactor pin definitions in pins_arduino.h

--- variants/retia-nibble/nibble-zero-connet/pins_arduino.h
+++ variants/retia-nibble/nibble-zero-connet/pins_arduino.h
@@ -7,11 +7,9 @@
 #define USB_VID 0x303a
 #define USB_PID 0x1001
 
-// I2C
 static const uint8_t SDA = 8;
 static const uint8_t SCL = 7;
 
-// SPI
 static const uint8_t SS = 10;
 static const uint8_t MOSI = 11;
 static const uint8_t MISO = 13;
@@ -22,11 +20,9 @@ static const uint8_t SCK = 12;
 #define SPI_MISO (13)
 #define SPI_CS (10)
 
-// Standard Green LED
 #define LED_BUILTIN (39)
 #define BUILTIN_LED (39)
 
-// NeoPixel (RGB)
 #undef RGB_BUILTIN
 #define RGB_BUILTIN (21)
 #define RGB_BRIGHTNESS 64
