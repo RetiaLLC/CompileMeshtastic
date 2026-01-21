f3b18976333a137422e6e4e549183efaafa44df5
Skickar <40251293+skickar@users.noreply.github.com>
2026-01-21 02:09:09 -0800

    Update variant.h

--- variants/retia-nibble/nibble-zero-connet/variant.h
+++ variants/retia-nibble/nibble-zero-connet/variant.h
@@ -1,38 +1,20 @@
-#ifndef _VARIANT_NIBBLE_ZERO_CONNECT_
-#define _VARIANT_NIBBLE_ZERO_CONNECT_
-
-// --- Display ---
 #define I2C_SDA 8
 #define I2C_SCL 7
-#define OLED_SDA I2C_SDA
-#define OLED_SCL I2C_SCL
-
-// --- LoRa Radio ---
 #define USE_SX1262
 #define LORA_SCK 12
 #define LORA_MISO 13
 #define LORA_MOSI 11
 #define LORA_CS 10
 #define LORA_RESET 6
-#define LORA_DIO1 4
-#define LORA_BUSY 5
-#define LORA_TXEN 3
-
 #ifdef USE_SX1262
-  #define SX126X_CS LORA_CS
-  #define SX126X_DIO1 LORA_DIO1
-  #define SX126X_BUSY LORA_BUSY
-  #define SX126X_RESET LORA_RESET
-  #define SX126X_DIO3_TCXO_VOLTAGE 1.8
+#define SX126X_CS LORA_CS
+#define SX126X_DIO1 4
+#define SX126X_BUSY 5
+#define SX126X_RESET LORA_RESET
+#define SX126X_DIO2_AS_RF_SWITCH
+#define SX126X_DIO3_TCXO_VOLTAGE 1.8
 #endif
-
-// --- Status LED ---
-// NOTE: PIN_LED1 is defined in platformio.ini to ensure it is visible to all files.
-// #define PIN_LED1 39 
-#define LED_ON 1 
-#define LED_INV 0 
-
-// --- Primary Button ---
+// #define PIN_LED1 39
 #define BUTTON_PIN 1
 #define BUTTON_NEED_PULLUP
 
